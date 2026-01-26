897322d1fd1a403002d70553513bd02fdd452add
Guillaume Coqueret <guillaume.coqueret@gmail.com>
2026-01-26 16:42:37 +0100

    up

--- server.js
+++ server.js
@@ -7,11 +7,6 @@ const PORT = process.env.PORT || 3000;
 // Serve static files from _build/site
 app.use(express.static(path.join(__dirname, '_build/site')));
 
-// SPA fallback - serve index.html for all routes
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '_build/site', 'index.html'));
-});
-
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
